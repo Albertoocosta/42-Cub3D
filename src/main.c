7004076e39e52a66b5248a0ade958ff13ecdf607
Carlos Alberto <albertoocostapt@gmail.com>
2025-08-26 02:27:26 +0100

    begin building raycasting

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: alberto <alberto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 11:17:40 by alberto           #+#    #+#             */
-/*   Updated: 2025/08/22 20:17:43 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/08/26 02:24:35 by alberto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(int ac, char **av)
 {
 	t_error		error_code;
+	t_cub		cub;
 
 	error_code = verify_arguments(ac, av);
 	if (error_code != SUCCESS)
@@ -23,5 +24,16 @@ int	main(int ac, char **av)
 		return (EXIT_FAILURE);
 	}
 	printf("Verificar arquivo log na root do repositório. Programa compilando e rodando sem erros!");
+	//inicia tudo a 0.
+	ft_bzero(&cub, sizeof(cub));
+	
+	/* ***** DEIXEI TUDO COMENTADO PARA AJUSTAR DEPOIS ****** */
+	
+	
+	//if (set_mlx(&cub))
+	//	return (1);
+	//falta criar as funcoes para os hooks
+	//mlx_loop_hook (cub.mlx.mlx_ptr, rendering, &cub);
+	//mlx_loop(cub.mlx.mlx_ptr);
 	return (EXIT_SUCCESS);
 }
---
+++ src/set_mlx.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_mlx.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alberto <alberto@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/25 13:34:50 by alberto           #+#    #+#             */
+/*   Updated: 2025/08/26 02:25:36 by alberto          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3D.h"
+
+int	set_mlx(t_cub *cub)
+{
+	(void)cub;
+/* 	cub->mlx.mlx_ptr = mlx_init();
+	if (cub->mlx.mlx_ptr == NULL)
+		return (ft_putstr_fd("Mlx error\n", 2), 1);
+	cub->mlx.win_ptr = mlx_new_window(cub->mlx.mlx_ptr, WIDTH, HEIGHT, 
+		WIN_TITLE);
+	if (cub->mlx.win_ptr == NULL)
+		return (ft_putstr_fd("Mlx error\n", 2), 1);
+	cub->mlx.img_ptr = mlx_new_image(cub->mlx.mlx_ptr, WIDTH, HEIGHT);
+	if (cub->mlx.img_ptr == NULL)
+		return (ft_putstr_fd("Mlx error\n", 2), 1);
+	cub->mlx.img_addr = mlx_get_data_addr(cub->mlx.img_ptr, 
+		&cub->mlx.bits_per_pixel, &cub->mlx.size_line, &cub->mlx.endian);
+	if (cub->mlx.img_addr == NULL)
+		return (ft_putstr_fd("Mlx error\n", 2), 1); */
+	return (0);	
+}
