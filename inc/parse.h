99af080110b1c57b32ed8560f9641c90a1525bf0
Rodrigo-fari <rodrigofal44@gmail.com>
2025-09-10 13:05:24 +0100

    Added new parse config logic.

--- inc/parse.h
+++ inc/parse.h
@@ -6,7 +6,7 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 19:58:11 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/09/10 12:42:09 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/09/10 13:03:39 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,16 +44,18 @@ typedef struct s_game
 	// Esta é a parte em que vamos ter que sincronizar com a estrutura que já existe no ficheiro 'cub3d.h'.
 } t_game;
 
-int parser(int ac, char **av, t_game *game);
-int check_extension(const char *file);
-void init_config(t_config *cfg);
-void init_map(t_map *map);
-void init_game(t_game *game);
+int		parser(int ac, char **av, t_game *game);
+int		check_extension(const char *file);
+void	init_config(t_config *cfg);
+void	init_map(t_map *map);
+void	init_game(t_game *game);
 
-int parse_input(const char *file, t_game *game);
-void parse_config(t_game *game, int fd);
-int parse_map(t_game *game, int fd);
+int		parse_input(const char *file, t_game *game);
+int		parse_config(t_game *game, int fd);
+int		parse_map(t_game *game, int fd);
 
-int parse_config_line(char *line, t_config *cfg);
+int		parse_config_line(char *line, t_config *cfg);
+int		parse_texture(char *line, char **path, bool *has_flag);
+
 
 #endif
--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 17:11:15 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/09/10 12:50:03 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/09/10 13:04:52 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,9 @@ int	parser(int ac, char **av, t_game *game)
 
 int	check_extension(const char *file)
 {
-	int len = ft_strlen(file);
+	int	len;
+
+	len = ft_strlen(file);
 	if (len < 4)
 		return (0);
 	return (ft_strncmp(file + len - 4, ".cub", 4) == 0);
@@ -39,61 +41,49 @@ int	check_extension(const char *file)
 
 int	parse_input(const char *file, t_game *game)
 {
-	int fd;
+	int	fd;
 
-	fd = 0;
 	fd = open(file, O_RDONLY);
 	if (fd == -1)
 		return (printf("Erro\nUnable to open map file"), 1);
-	parse_config(game, fd);
-	// parse_map(game, fd);
+	if (parse_config(game, fd)) // Futuramente adicionar ao if o "parse_map(game, fd)"
+		return (1);
 	close(fd);
 	return (0);
 }
 
-void	parse_config(t_game *game, int fd)
+int	parse_config(t_game *game, int fd)
 {
 	char *line;
-	int config = 0;
-	int mapl = 0;
 
 	line = NULL;
 	while ((line = get_next_line(fd)))
 	{
-		if (ft_strncmp(line, "NO ", 3) == 0 || ft_strncmp(line, "SO ", 3) == 0 || ft_strncmp(line, "WE ", 3) == 0 || ft_strncmp(line, "EA ", 3) == 0 || ft_strncmp(line, "C ", 2) == 0 || ft_strncmp(line, "F ", 2) == 0)
-			config++;
-		// parse_config_line(line, &game->config);
-		else if (line[0] == '1' || line[0] == '0' || line[0] == ' ')
-			mapl++;
+		if (parse_config_line(line, &game->config))
+			return (1);
 		free(line);
 	}
-	printf("CONFIG LINES: %d.\nMAP LINES: %d.\n", config, mapl);
-	(void)game;
+	return (0);
 }
 
-// int	parse_map(t_game *game, int	fd)
-// {
-
-// }
-
-// int	parse_config_line(char *line, t_config *cfg)
-// {
-// 	while (*line == ' ' || *line == '\t')
-// 		line++;
-// 	if (ft_strncmp(line, "NO ", 3) == 0)
-// 		return parse_texture(line + 2, &cfg->no_path, &cfg->has_no);
-// 	if (ft_strncmp(line, "SO ", 3) == 0)
-// 		return parse_texture(line + 2, &cfg->so_path, &cfg->has_so);
-// 	if (ft_strncmp(line, "WE ", 3) == 0)
-// 		return parse_texture(line + 2, &cfg->we_path, &cfg->has_we);
-// 	if (ft_strncmp(line, "EA ", 3) == 0)
-// 		return parse_texture(line + 2, &cfg->ea_path, &cfg->has_ea);
-// 	if (ft_strncmp(line, "F ", 2) == 0)
-// 		return parse_color(line + 1, cfg->floor_rgb, &cfg->has_floor);
-// 	if (ft_strncmp(line, "C ", 2) == 0)
-// 		return parse_color(line + 1, cfg->ceil_rgb, &cfg->has_ceil);
-// 	return (0);
-// }
+int	parse_config_line(char *line, t_config *cfg)
+{
+	while (*line == ' ' || *line == '\t')
+		line++;
+	if (ft_strncmp(line, "NO ", 3) == 0)
+		return parse_texture(line + 2, &cfg->no_path, &cfg->has_no);
+	if (ft_strncmp(line, "SO ", 3) == 0)
+		return parse_texture(line + 2, &cfg->so_path, &cfg->has_so);
+	if (ft_strncmp(line, "WE ", 3) == 0)
+		return parse_texture(line + 2, &cfg->we_path, &cfg->has_we);
+	if (ft_strncmp(line, "EA ", 3) == 0)
+		return parse_texture(line + 2, &cfg->ea_path, &cfg->has_ea);
+	// if (ft_strncmp(line, "F ", 2) == 0)
+	// 	return parse_color(line + 1, cfg->floor_rgb, &cfg->has_floor);
+	// if (ft_strncmp(line, "C ", 2) == 0)
+	// 	return parse_color(line + 1, cfg->ceil_rgb, &cfg->has_ceil);
+	return (0);
+}
 
 int	parse_texture(char *line, char **path, bool *has_flag)
 {
@@ -112,3 +102,8 @@ int	parse_texture(char *line, char **path, bool *has_flag)
 	*has_flag = true;
 	return (0);
 }
+
+// int	parse_map(t_game *game, int	fd)
+// {
+
+// }
