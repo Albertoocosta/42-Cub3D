2c7e32a158196cc27fae883985bbfbc67606d591
Rodrigo-fari <rodrigofal44@gmail.com>
2025-09-11 13:31:25 +0100

    Pushing branch to remote repository.

--- inc/parse.h
+++ inc/parse.h
@@ -6,7 +6,7 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 19:58:11 by rde-fari          #+#    #+#             */
-/*   Updated: 2025/09/11 13:13:18 by rde-fari         ###   ########.fr       */
+/*   Updated: 2025/09/11 13:31:20 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int parser(int ac, char **av, t_cub *cub);
 int check_extension(const char *file, char *extension);
 int parse_input(const char *file, t_cub *cub);
 int parse_config(t_cub *cub, int fd);
-int validate_config(t_cub *cub);
+int	validate_config(t_cub *cub);
 int parse_config_line(char *line, t_texture *texture);
 int parse_texture(char *line, char **path, bool *has_flag);
 
